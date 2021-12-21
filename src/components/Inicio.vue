aacea9b8254ec5515caf07790e15dcf9cb673a07
AndyRCR <andycr321@gmail.com>
2021-12-21 16:12:37 -0500

    first commit

---
+++ src/components/Inicio.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="animate__animated animate__backInLeft">
+    <h3 style="color: #08fdd8">Bienvenid@!</h3>
+    <p>
+      Hola, soy Andy Canales, desarrollador web front-end. ¿Tienes una idea de negocio o una
+      problemática que resolver?. Ponte en contacto conmigo a
+      través de las siguientes redes.
+    </p>
+    <a
+      href="https://www.linkedin.com/in/andy-ruben-canales-roman-818a39219/"
+    >
+      <img src="@/assets/icons/linkedin.png" />
+    </a>
+    <a href="https://github.com/AndyRCR">
+      <img src="@/assets/icons/github.png" />
+    </a>
+    <a href="https://twitter.com/RubenCanalesR">
+      <img src="@/assets/icons/twitter.png" />
+    </a>
+  </div>
+</template>
+<style scoped>
+* {
+  color: white;
+}
+h3 {
+  font-size: 50px;
+}
+p {
+  font-size: 25px;
+}
+img:hover {
+  transform: scale(1.5);
+}
+</style>
