afe0ed46d9c770cd5e2c33cea628104369fa87d8
AndyRCR <andycr321@gmail.com>
2022-06-30 13:12:01 -0500

    Modifying about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -18,7 +18,7 @@
             Soy un estudiante de la carrera de Ingeniería de Sistemas, con un gran interés
             en el area del desarrollo web Front-end.
             <br><br>
-            He tenido experiencia como Líder de Proyectos/Analista de Datos, y en el rubro de la programación web como desarrollador
+            He tenido experiencia como Líder de Proyectos/Analista Programador, y en el rubro de la programación web como desarrollador
             Front-end freelance; cualquier duda o consulta tambien puede realizarlo por mi correo o número telefonico:
             <a href="https://api.whatsapp.com/send/?phone=51970811868&text&app_absent=0" style="color: #08fdd8">andycr321@gmail.com</a> / 
             <a href="https://andyrcr.github.io/MusiCSStore---CEA/" style="color: #08fdd8" target="_blank"> +51 970811868</a>
